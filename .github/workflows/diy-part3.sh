3fd82b20d741c8debe1e8c860f826ac20ebbe232
huntersonzjg <78995644+huntersonzjg@users.noreply.github.com>
2024-09-21 13:24:36 +0800

    Update diy-part3.sh

--- .github/workflows/diy-part3.sh
+++ .github/workflows/diy-part3.sh
@@ -16,5 +16,5 @@
 # Add a feed source
 #echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git modemfeed https://github.com/koshev-msk/modemfeed.git' >>feeds.conf.default
-echo 'src-git kenzok8 https://github.com/kenzok8/small-package.git ' >>feeds.conf.default  
+#echo 'src-git kenzok8 https://github.com/kenzok8/small-package.git ' >>feeds.conf.default  
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default    
